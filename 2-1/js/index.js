463cf03e5c42a6ddebb5b70cd0951a0035aa5bf6
hpbdmylove <iammaytinhdibo@gmail.com>
2018-03-18 14:32:47 +0700

    new render model

--- 2-1/js/index.js
+++ 2-1/js/index.js
@@ -27,21 +27,35 @@ if (localStorage.datatodo == null) {
     localStorage.datatodo = JSON.stringify(datasample);
 }
 
-function setEvent(target){
+jsonData = JSON.parse(localStorage.datatodo);
+
+function setEvent(target) {
     target.addEventListener("click", function (e) {
         if (e.target.tagName != "BUTTON") {
             done(e.currentTarget);
         } else {
-            e.target.parentElement.remove();
+            jsonData.splice(e.target.parentElement.id,1);
+            localStorage.datatodo = JSON.stringify(jsonData);
+            loadFromStorage();
             alertText("Removed!");
-            loadevent();
         }
     });
 }
+
+function checklength() {
+    if (todos.length == 0) {
+        document.getElementById("empty").style.display = "block";
+    } else {
+        document.getElementById("empty").style.display = "none";
+    }
+}
+
 function loadFromStorage() {
+    document.getElementById("list").innerHTML='<div id="empty"> <br/> <img src="img/empty.png"/> <span>This list is empty!</span> </div>';
     data = JSON.parse(localStorage.datatodo);
     for (i = 0; i < data.length; i++) {
         listHTML = document.createElement("li");
+        listHTML.id = i;
         setEvent(listHTML);
         if (data[i].status == 1) {
             listHTML.className = "done";
@@ -51,6 +65,7 @@ function loadFromStorage() {
         listHTML.innerHTML += '<span>' + data[i].title + '</span><button class="remove">&#x2715;</button>';
         document.getElementById("list").appendChild(listHTML);
     }
+    checklength();
 }
 
 loadFromStorage();
@@ -62,61 +77,40 @@ function alertText(text) {
 }
 
 function done(target) {
-    console.log(target);
     if (target.tagName != "LI") {
         target = target.parentElement;
     }
     if (target.className == "done") {
         target.className = "none";
+        jsonData[target.id].status=0;
+        alertText("Undone!");
     } else {
         target.className = "done";
-    }
-    alertText("Done!");
-    for (i = 0; i < todos.length; i++) {
-        jsonData[i].status = todos[i].className == "done" ? 1 : 0;
+        jsonData[target.id].status=1;
+        alertText("Done!");
     }
     localStorage.datatodo = JSON.stringify(jsonData);
 }
-
-function checklength() {
-    if (todos.length == 0) {
-        document.getElementById("empty").style.display = "block";
-    } else {
-        document.getElementById("empty").style.display = "none";
-    }
-}
-function loadevent() {
-    jsonData = [];
-    checklength();
-    for (i = 0; i < todos.length; i++) {
-        jsonData[i] = Object.create(struct);
-        jsonData[i].title = todos[i].getElementsByTagName("span")[0].innerText;
-        jsonData[i].status = todos[i].className == "done" ? 1 : 0;
-    }
-    localStorage.datatodo = JSON.stringify(jsonData);
-}
-
-loadevent();
 
 function addnew() {
     if (textInput.value.length < 1) {
         alertText("Your text is empty!");
     } else {
-        var child = document.createElement("li");
-        setEvent(child);
-        child.className = "none";
-        child.innerHTML = '<span>' + textInput.value + '</span><button class="remove">&#x2715;</button>';
-        document.getElementById("list").appendChild(child);
+        var next = {
+            title: textInput.value,
+            status: 0
+        }
+        jsonData.push(next);
+        localStorage.datatodo = JSON.stringify(jsonData);
+        loadFromStorage();
         textInput.value = '';
         alertText("New reminder has been added!");
     }
-    loadevent();
 }
 
 textInput.addEventListener("keypress", function (e) {
     if (e.keyCode == 13) {
         addnew();
-        loadevent();
     };
 });
 
